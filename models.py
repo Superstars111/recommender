c32c5b8cd6028dbbdfca99586902458436e800c7
Superstars111 <jared.superstars111@gmail.com>
2022-10-20 16:23:20 -0700

    Added basic table structures

--- models.py
+++ models.py
@@ -3,6 +3,38 @@ from sqlalchemy.orm import relationship, backref
 from flask_login import UserMixin, LoginManager
 from .app import db
 
+login = LoginManager()
+
 
 class User(UserMixin, db.Model):
-    pass
+    __tablename__ = "users"
+
+    id = sqa.Column(sqa.Integer(), primary_key=True, index=True)
+    email = sqa.Column(sqa.String(), unique=True, nullable=False)
+    username = sqa.Column(sqa.String(), unique=True, nullable=False)
+    password_hash = sqa.Column(sqa.String(), nullable=False)
+
+
+class Media(db.Model):
+    __tablename__ = "media"
+
+    id = sqa.Column(sqa.Integer(), primary_key=True, index=True)
+    title = sqa.Column(sqa.String(), nullable=False)
+    type = sqa.Column(sqa.Integer(), nullable=False)  # Book, movie, anime, manga, video game, webcomic, song, other
+    version_specific = sqa.Column(sqa.Boolean())  # e.g., "I recommend SPECIFICALLY the anime, not the manga"
+    # length
+    # cost
+
+
+class Recommendation(db.Model):
+    __tablename__ = "recommendations"
+
+    id = sqa.Column(sqa.Integer(), primary_key=True, index=True)
+
+    # Sender
+    sent_by = sqa.Column(sqa.Integer(), sqa.ForeignKey("users.id"))
+    sender_priority = sqa.Column(sqa.Integer())
+
+    # Receiver
+    sent_to = sqa.Column(sqa.Integer(), sqa.ForeignKey("users.id"), nullable=False)
+    personal_priority = sqa.Column(sqa.Integer())
